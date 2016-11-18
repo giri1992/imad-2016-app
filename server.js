ccaf242fe6bd973e2c01181ef4959585ed774b72
giri1992 <dharan1992@gmail.com>
2016-11-18 18:02:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'giri1992',
+    database: 'giri1992',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -47,6 +57,21 @@ var articles = {
     },
     
 };
+
+
+var pool = new pool(config);
+app.get('/test-db', function(req,res) {
+    pool.query('SELECT * FROM test', function (err,result) {
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringify(result));
+        }
+    });
+});
+
+
+
 
 function createTemplate(data) {
     var title = data.title;
