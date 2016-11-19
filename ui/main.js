713d4a51b93dc6250a208566076fe600b0b8fdc6
giri1992 <dharan1992@gmail.com>
2016-11-19 18:42:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,7 @@ function loadLoginForm () {
         <h3>Login/Register to unlock awesome features</h3>
         <input type="text" id="username" placeholder="username" />
         <br />
+        <br/>
         <input type="password" id="password" />
         <br/><br/>
         <input type="submit" id="login_btn" value="Login" />
